2c8973a89272ea9e70b9cb21a8bf4f786cf46480
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2019-02-11 16:44:27 +0100

    Bump version: 0.0.19 → 0.0.20

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/anonapi',
-    version='0.0.19',
+    version='0.0.20',
     zip_safe=False,
 )
