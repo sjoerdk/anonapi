18e3ed7d72cb8cdd801ceca5450d1332188c403c
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-07-23 15:53:07 +0200

    Bump version: 0.0.31 → 0.0.32

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/anonapi',
-    version='0.0.31',
+    version='0.0.32',
     zip_safe=False,
 )
