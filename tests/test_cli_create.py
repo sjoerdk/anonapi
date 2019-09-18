bad6d69819ddf0413c16b78da680455dc2e16953
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-09-18 15:40:15 +0200

    Refactors test_cli_create

--- tests/test_cli_create.py
+++ tests/test_cli_create.py
@@ -40,6 +40,14 @@ def mock_from_mapping_runner(mock_main_runner_with_mapping):
     return mock_main_runner_with_mapping
 
 
+@pytest.fixture()
+def mock_requests_for_job_creation(mock_requests):
+    """Mock requests library so that every call to it will return a standard anonapi Job created response
+    """
+    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
+    return mock_requests
+
+
 def test_create_from_mapping_no_mapping(mock_main_runner):
     """Running from-mapping in a folder without mapping should not work"""
     result = mock_main_runner.invoke(main, "from-mapping")
@@ -47,22 +55,21 @@ def test_create_from_mapping_no_mapping(mock_main_runner):
     assert "No mapping" in result.output
 
 
-def test_create_from_mapping(mock_from_mapping_runner, mock_requests):
+def test_create_from_mapping(mock_from_mapping_runner, mock_requests_for_job_creation):
     """Try from-mapping, creating jobs based on a mapping"""
 
     # Run and answer are you sure 'N'
     result = mock_from_mapping_runner.invoke(main, "from-mapping", input="N")
     assert result.exit_code == 0
     assert "Cancelled" in result.output
-    assert mock_requests.requests.post.call_count == 0
+    assert mock_requests_for_job_creation.requests.post.call_count == 0
 
     # Now answer Y
-    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
     result = mock_from_mapping_runner.invoke(
         main, "from-mapping", input="Y", catch_exceptions=False
     )
     assert result.exit_code == 0
-    assert mock_requests.requests.post.call_count == 2
+    assert mock_requests_for_job_creation.requests.post.call_count == 20
     batch_folder = BatchFolder(mock_from_mapping_runner.mock_context.current_dir())
     assert batch_folder.has_batch()
     assert batch_folder.load().job_ids == [
@@ -109,14 +116,14 @@ def test_create_from_mapping_server_error_halfway(
     assert batch_folder.load().job_ids == [1234]
 
 
-def test_create_set_default_parameters(mock_main_runner_with_mapping, mock_requests):
+def test_create_set_default_parameters(mock_main_runner_with_mapping, mock_requests_for_job_creation):
     # Try to run from-mapping
     result = mock_main_runner_with_mapping.invoke(main, "from-mapping", input="Y")
 
     # This should not work because essential settings are missing
     assert result.exit_code == 0
     assert "Could not find default project name" in result.output
-    assert mock_requests.requests.post.call_count == 0
+    assert mock_requests_for_job_creation.requests.post.call_count == 0
 
     # set them
     result = mock_main_runner_with_mapping.invoke(
@@ -125,10 +132,9 @@ def test_create_set_default_parameters(mock_main_runner_with_mapping, mock_reque
     assert result.exit_code == 0
 
     # Now this command should succeed
-    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
     result = mock_main_runner_with_mapping.invoke(main, "from-mapping", input="Y")
     assert result.exit_code == 0
-    assert mock_requests.requests.post.call_count == 20
+    assert mock_requests_for_job_creation.requests.post.call_count == 20
 
 
 def test_show_set_default_parameters(mock_main_runner):
@@ -142,17 +148,16 @@ def test_show_set_default_parameters(mock_main_runner):
     assert all(x in result.output for x in ["test_project", "test_destination"])
 
 
-def test_create_from_mapping_relative_path(mock_from_mapping_runner, mock_requests):
+def test_create_from_mapping_relative_path(mock_from_mapping_runner, mock_requests_for_job_creation):
     """Source identifiers in mappings are usually given as relative paths. However, jobs should be created with
     absolute, unambiguous paths. Check that this conversion works
 
     """
-    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
     result = mock_from_mapping_runner.invoke(
         main, "from-mapping", input="Y", catch_exceptions=False
     )
     assert result.exit_code == 0
-    assert mock_requests.requests.post.call_count == 20
+    assert mock_requests_for_job_creation.requests.post.call_count == 20
 
     current_dir = str(mock_from_mapping_runner.mock_context.current_dir())
     batch_folder = BatchFolder(current_dir)
@@ -161,35 +166,29 @@ def test_create_from_mapping_relative_path(mock_from_mapping_runner, mock_reques
     # in mapping there should be no mention of the current dir
     assert not any([current_dir in x for x in paths_in_mapping])
     # But in the created jobs the current dir should have been added
-    assert current_dir in str(mock_requests.requests.post.call_args)
+    assert current_dir in str(mock_requests_for_job_creation.requests.post.call_args)
 
 
-def test_create_from_mapping_dry_run(mock_from_mapping_runner, mock_requests):
+def test_create_from_mapping_dry_run(mock_from_mapping_runner, mock_requests_for_job_creation):
     """Test dry run mode. Should not hit anything important
     """
-    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
+
     result = mock_from_mapping_runner.invoke(
         main, "from-mapping --dry-run", input="Y", catch_exceptions=False
     )
     assert result.exit_code == 0
     assert "Dry run" in result.output
     assert "patient4" in result.output
-    assert mock_requests.requests.post.call_count == 0
+    assert mock_requests_for_job_creation.requests.post.call_count == 0
 
 
-def test_create_from_mapping_folder_and_pacs(mock_from_mapping_runner, a_folder_with_mapping_diverse, mock_requests):
+def test_create_from_mapping_folder_and_pacs(mock_from_mapping_runner,
+                                             a_folder_with_mapping_diverse,
+                                             mock_requests_for_job_creation):
     """PACS identifiers should generate slightly different jobs then Folder identifiers."""
-
+    mock_requests = mock_requests_for_job_creation
     mock_from_mapping_runner.set_mock_current_dir(a_folder_with_mapping_diverse)
 
-    # Run and answer are you sure 'N'
-    result = mock_from_mapping_runner.invoke(main, "from-mapping", input="N")
-    assert result.exit_code == 0
-    assert "Cancelled" in result.output
-    assert mock_requests.requests.post.call_count == 0
-
-    # Now answer Y
-    mock_requests.set_response(RequestsMockResponseExamples.JOB_CREATED_RESPONSE)
     result = mock_from_mapping_runner.invoke(
         main, "from-mapping", input="Y", catch_exceptions=False
     )
