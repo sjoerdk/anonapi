bf63a9baadc0982f7dc26b192ab36a057fe36b95
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-10-02 15:56:58 +0200

    Improves tests for inputfile functionality

--- anonapi/inputfile.py
+++ anonapi/inputfile.py
@@ -5,7 +5,7 @@ and optionally pseudonyms.
 It is often easier to add such a file to a mapping then to alter-copy-paste between
 files
 """
-from typing import Iterator, List, Type, Union
+from typing import Iterator, List, Type, Union, Optional
 
 from openpyxl.reader.excel import load_workbook
 
@@ -16,6 +16,7 @@ from anonapi.parameters import (
     Parameter,
     ParameterFactory,
     ParameterParsingError,
+    PathParameter,
     PatientName,
 )
 
@@ -32,15 +33,19 @@ class ParameterColumn:
     # the type of parameter that this column contains
     parameter_type: Type[Parameter] = Parameter
 
-    def __init__(self, column: int):
+    def __init__(self, column: int, header_row_idx: int = 0):
         """
 
         Parameters
         ----------
-        column: Optional[int]
+        column: int
             The 0-based column in which this header instance is found
+        header_row_idx: int, optional
+            The 0-based row where this column's header is found. For better
+            error messages (print row in file where an error occurred)
         """
         self.column = column
+        self.header_row_idx = header_row_idx
 
     def __str__(self) -> str:
         if self.header_names:
@@ -49,7 +54,7 @@ class ParameterColumn:
             return "Column"
 
     @staticmethod
-    def clean_string(string):
+    def clean_string(string: str):
         """Make lowercase and remove separators"""
         return (
             string.replace(" ", "")
@@ -68,6 +73,14 @@ class ParameterColumn:
             return True
         else:
             return False
+
+    @classmethod
+    def header_name(cls) -> str:
+        """A header name that his header might have. For helpful error messages"""
+        if cls.header_names:
+            return cls.header_names[0]
+        else:
+            return ""
 
     def parameter_from_row(self, row: List[str]) -> Parameter:
         """Try to parse a Parameter instance out of a row from the grid
@@ -88,7 +101,8 @@ class ParameterColumn:
 
     def has_empty_value(self, row: List[str]) -> bool:
         """True if the given row has no value in this column"""
-        return row[self.column] == ""
+
+        return row[self.column] == "" or row[self.column] is None
 
 
 class AccessionNumberColumn(ParameterColumn):
@@ -103,6 +117,12 @@ class PseudonymColumn(ParameterColumn):
     parameter_type: Type[Parameter] = PatientName
 
 
+class FolderColumn(ParameterColumn):
+
+    header_names = ["folder", "map", "path"]
+    parameter_type: Type[Parameter] = PathParameter
+
+
 def parse_columns(
     row: List[str], column_types: List[Type[ParameterColumn]]
 ) -> List[ParameterColumn]:
@@ -114,6 +134,7 @@ def parse_columns(
         The row of values to parse
     column_types: List[Type[ParameterColumn]]
         The types of columns to try
+
     """
     columns = []
 
@@ -130,54 +151,117 @@ def find_column_headers(
 ) -> List[ParameterColumn]:
     """Go through each row in iterator until you find a row containing column headers
 
+    Parameters
+    ----------
+    row_iterator: Iterator[List[str]]
+        iterator returning lists of strings corresponding to each row of a
+        grid-like file
+    column_types: List[Type[ParameterColumn]]
+        The types of columns to look for
+
+    Returns
+    -------
+    List[ParameterColumn]
+        All parameter columns found
+
     Raises
     ------
     InputFileException
         When no column headers can be found
-    column_types: List[Type[ParameterColumn]]
-        The types of columns to look for
+
     """
-    for row in row_iterator:
+
+    for idx, row in enumerate(row_iterator):
         columns = parse_columns(row, column_types=column_types)
         if columns:
-            #  TODO: print where in files headers are found. Probably use logger
+            # add column header row idx for better error messages later
+            for column in columns:
+                column.header_row_idx = idx
             return columns
-    raise InputFileException("Could not find ")
+    raise InputFileException(
+        f"Could not find any column headers. Looked for any"
+        f" of [{','.join([x.header_name() for x in column_types])}]"
+    )
 
 
-def read_file(file_path: str) -> JobParameterGrid:
-    """Read an xls file to extract parameter_types for jobs"""
+def cast_rows_to_string(iterator: Iterator[List]) -> Iterator[List[Optional[str]]]:
+    """For standardizing data from grid-like files. Make everything string,
+    except None values. Keep those None.
+
+    """
+
+    def str_preserve_none(input):
+        if input is None:
+            return None
+        else:
+            return str(input)
+
+    for row in iterator:
+        yield list(map(str_preserve_none, row))
+
+
+def extract_parameter_grid(
+    file_path: str, column_types: List[Type[ParameterColumn]] = None
+) -> JobParameterGrid:
+    """Read an xls file and try to extract a grid of parameters
+
+    Parameters
+    ----------
+    file_path: str
+        Extract from this file
+    column_types: List[Type[ParameterColumn]], optional
+        Search for these column types. Defaults to AccessionNumber and Pseudonym
+
+
+    Raises
+    ------
+    InputFileException
+        If parsing or reading fails for any reason
+
+    """
     # read in xlsx reader
-
-    column_types = [AccessionNumberColumn, PseudonymColumn]
+    if column_types is None:
+        column_types = [AccessionNumberColumn, PseudonymColumn]
 
     wb2 = load_workbook(file_path)
     sheet = wb2[wb2.sheetnames[0]]
 
-    row_iterator = sheet.values
+    row_iterator = cast_rows_to_string(sheet.values)
+
     columns = find_column_headers(row_iterator, column_types=column_types)
+
+    column_headers_idx = columns[0].header_row_idx
 
     # column headers found. Build a grid one row at a time
     grid = []
-    for row in row_iterator:
+    for idx, row in enumerate(row_iterator):
         try:
-            check_row(row, columns=columns)
+            grid.append(parse_row(row, columns=columns))
         except EmptyRow:
             continue  # skip empty row, try next
-        except PartiallyEmptyRow:
-            # this is a problem. Raise further
-            raise
-        # checking was OK, parse
-        try:
-            grid.append([column.parameter_from_row(row) for column in columns])
-        except ParameterParsingError as e:
-            InputFileParseException(f"Error parsing row {row}: {e}")
+        except RowParseException as e:
+            # Add row number (+2 as excel rows start at 1 and both idxs are 0-based)
+            raise InputFileParseException(
+                f"Exception in row {column_headers_idx+idx+2}: {e}"
+            )
 
     return JobParameterGrid(grid)
 
 
-def check_row(row: List[str], columns: List[ParameterColumn]):
-    """Is this row fit for parsing?
+def parse_row(row: List[str], columns: List[ParameterColumn]) -> List[Parameter]:
+    """Is this row fit for parsing? Are there missing parameters? empty values?
+
+    Parameters
+    ----------
+    row: List[str]
+        String value of each cell in this row
+    columns: List[ParameterColumn])
+        The column names for each value in row
+
+    Returns
+    -------
+    List[Parameter]
+        Each value in row parsed according to column type
 
     Raises
     ------
@@ -185,6 +269,8 @@ def check_row(row: List[str], columns: List[ParameterColumn]):
         When row is empty
     PartiallyEmptyRow
         When some columns are filled but not all
+    InputFileParseException
+        When any of the values in row cannot be parsed according to their columns
     """
     filled = []
     empty = []
@@ -197,10 +283,11 @@ def check_row(row: List[str], columns: List[ParameterColumn]):
     if not filled:
         raise EmptyRow()
     if filled and empty:
-        raise PartiallyEmptyRow(
+        raise RowParseException(
             f"Problem in row {row}. Columns[{[str(x) for x in filled]}] have a value,"
             f" but columns [{[str(x) for x in empty]}] are empty. What do you want?"
         )
+    return [column.parameter_from_row(row) for column in columns]
 
 
 def columns_are_all_empty(row: List[str], columns: List[ParameterColumn]) -> bool:
@@ -224,5 +311,5 @@ class EmptyRow(AnonAPIException):
     pass
 
 
-class PartiallyEmptyRow(AnonAPIException):
+class RowParseException(AnonAPIException):
     pass
