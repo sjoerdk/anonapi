fc88a973f7105519ba73565dbff9b435629e508c
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-10-29 23:07:47 +0100

    Bump version: 1.5.1 → 1.5.2

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjoerdk/anonapi",
-    version="1.5.1",
+    version="1.5.2",
     zip_safe=False,
 )
